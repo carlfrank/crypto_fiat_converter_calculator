f0099dd4100862d84ecdbbe531e68dec7ad0df7a
Carlos <carl-frank7@hotmail.com>
2024-03-30 20:50:17 -0400

    Delete currency conversion functionality

--- routes/currencyRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { validateCurrencyConversion } = require('./validators');
-const { convertCurrencies } = require('./convertController');
-
-const router = express.Router();
-
-router.get('/convert', validateCurrencyConversion, convertCurrencies);
-
-module.exports = router;
